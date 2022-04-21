8805c79c56c00e90732e039318fbebb60d00de1a
tmafaralala <tmafaralala@jhb.dvt.co.za>
2022-04-21 23:30:29 +0200

    moved files around

---
+++ Geo-Weather/Geo-WeatherTests/Geo_WeatherTests.swift
@@ -0,0 +1,13 @@
+//
+//  Geo_WeatherTests.swift
+//  Geo-WeatherTests
+//
+//  Created by Tshwarelo Mafaralala on 2022/04/21.
+//
+
+import XCTest
+@testable import Geo_Weather
+
+class GeoWeatherTests: XCTestCase {
+
+}
