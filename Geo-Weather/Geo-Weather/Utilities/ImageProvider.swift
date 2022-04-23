5a21e1c2393b007508b6436b447a7194ec76a6ec
tmafaralala <tmafaralala@jhb.dvt.co.za>
2022-04-23 22:00:06 +0200

    refined custom colors declaration

---
+++ Geo-Weather/Geo-Weather/Utilities/ImageProvider.swift
@@ -0,0 +1,19 @@
+//
+//  ImageProvider.swift
+//  Geo-Weather
+//
+//  Created by Tshwarelo Mafaralala on 2022/04/23.
+//
+
+import Foundation
+import UIKit
+
+struct ImageProvider {
+    var cloudy: UIImage {
+        switch(ThemeProvider.theme){
+        case .sea {
+                
+            }
+        }
+    }
+}
