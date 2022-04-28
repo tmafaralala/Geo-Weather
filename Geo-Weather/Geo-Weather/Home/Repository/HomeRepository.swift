9ba4d09bf865901da5980afdb0b69805d4fcc40b
tmafaralala <tmafaralala@jhb.dvt.co.za>
2022-04-28 09:10:52 +0200

    updated image storyboard reference

--- Geo-Weather/Geo-Weather/Home/Repository/HomeRepository.swift
+++ Geo-Weather/Geo-Weather/Home/Repository/HomeRepository.swift
@@ -11,20 +11,28 @@ typealias CurrentWeatherResponse = ApiClientResponse<GeoWeather>
 typealias ForecastWeatherResponse = ApiClientResponse<GeoWeatherForecast>
 
 protocol HomeRepositoryType: AnyObject {
-    func fetchCurrentWeatherData(completion: @escaping(CurrentWeatherResponse))
-    func fetchForecastWeatherData(completion: @escaping (ForecastWeatherResponse))
+    func fetchCurrentWeatherData(lat: Double,
+                                 lon: Double,
+                                 completion: @escaping(CurrentWeatherResponse))
+    func fetchForecastWeatherData(lat: Double,
+                                  lon: Double,
+                                  completion: @escaping (ForecastWeatherResponse))
 }
 
 class HomeRepository: HomeRepositoryType {
-    func fetchCurrentWeatherData(completion: @escaping (CurrentWeatherResponse)) {
-        let homeRequest = WeatherRequest()
+    func fetchCurrentWeatherData(lat: Double,
+                                 lon: Double,
+                                 completion: @escaping (CurrentWeatherResponse)) {
+        let homeRequest = WeatherRequest(lat: lat, lon: lon)
         ApiClient.shared.call(with: homeRequest, for: GeoWeather.self) { result in
             completion(result)
         }
     }
     
-    func fetchForecastWeatherData(completion: @escaping (ForecastWeatherResponse)) {
-        let homeRequest = WeatherRequest()
+    func fetchForecastWeatherData(lat: Double,
+                                  lon: Double,
+                                  completion: @escaping (ForecastWeatherResponse)) {
+        let homeRequest = WeatherRequest(lat: lat, lon: lon)
         homeRequest.urlString = Path.forecast
         ApiClient.shared.call(with: homeRequest, for: GeoWeatherForecast.self) { result in
             completion(result)
